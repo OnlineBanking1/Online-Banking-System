390a27df630e43d2becbb71449d0123bd4c9db95
rahul7291 <rahulagrawal.070291@gmail.com>
2014-05-16 16:07:58 -0400

    Test Rahul1.0

--- OnlineBanking/src/main/java/com/banking/app/dao/CustomerDaoImpl.java
+++ OnlineBanking/src/main/java/com/banking/app/dao/CustomerDaoImpl.java
@@ -1,10 +1,11 @@
 package com.banking.app.dao;
 
 import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import com.banking.app.model.Customer;
 
-public class CustomerDaoImpl implements CustomerDao {
+public class CustomerDaoImpl  implements CustomerDao {
 
 	private SessionFactory sessionFactory;
 	
--- OnlineBanking/src/main/java/com/banking/app/service/CustomerServiceImpl.java
+++ OnlineBanking/src/main/java/com/banking/app/service/CustomerServiceImpl.java
@@ -1,9 +1,12 @@
 package com.banking.app.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.banking.app.dao.CustomerDao;
 import com.banking.app.model.Customer;
-
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
+	
 	private CustomerDao customerDao; 
 public void addCustomer(Customer customer)
 {
